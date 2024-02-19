3e4cbc6c7c6c17323780e156f4c09f5e7570473e
SirJupiter <jupitertoby@gmail.com>
2024-02-19 14:19:23 +0100

    0. Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,14 +9,13 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t iterator_a, iterator_b;
 	int temp;
-	bool swapped;
+	bool swapped = false;
 
 	if (size < 2)
 		return;
 
 	for (iterator_a = 0; iterator_a < size - 1; iterator_a++)
 	{
-		swapped = false;
 		for (iterator_b = 0; iterator_b < size - 1 - iterator_a; iterator_b++)
 		{
 			if (array[iterator_b] > array[iterator_b + 1])
@@ -24,11 +23,9 @@ void bubble_sort(int *array, size_t size)
 				temp = array[iterator_b];
 				array[iterator_b] = array[iterator_b + 1];
 				array[iterator_b + 1] = temp;
+				print_array(array, size);
 				swapped = true;
 			}
-
-			if (swapped == true)
-				print_array(array, size);
 		}
 
 		if (swapped == false)
